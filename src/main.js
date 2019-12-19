f61a47e2edfc37e140ca79511e08682facc0b97d
xw-web <1272716615@qq.com>
2019-12-19 16:53:15 +0800

    引入elementui和echarts

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Echarts from 'echarts'
+
+Vue.use(ElementUI)
+Vue.prototype.$echarts = Echarts
 
 Vue.config.productionTip = false
 
---
+++ src/pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    我是首页
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
