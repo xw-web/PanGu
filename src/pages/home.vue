29ff0b335c0b6ba6cc410150b369ae9aff8ae852
xw-web <1272716615@qq.com>
2019-12-29 22:15:43 +0800

    修改

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,34 +1,19 @@
 <template>
   <div>
     <header class="home_header">
-      <div class="header_main flex_row">
-        <div class="header_first">1</div>
-        <div class="header_second">
-          <ul class="navList_ul clear">
-            <li class="navList_li float_left" v-for="(item,index) in navList" :key="index">
-              <span>{{item.name}}</span>
-            </li>
-          </ul>
-        </div>
-        <div class="header_third">3</div>
+      <div class="header_main">
+        
       </div>
     </header>
     <router-view></router-view>
   </div>
 </template>
-
 <script>
 export default {
   name:'Home',
   data(){
     return {
-      navList:[
-        {id:'1',name:'Echarts'},
-        {id:'2',name:'Echarts'},
-        {id:'3',name:'Echarts'},
-        {id:'4',name:'Echarts'},
-        {id:'5',name:'Echarts'}
-      ]
+
     }
   },
   methods:{
@@ -45,26 +30,5 @@ export default {
 }
 .header_main {
   width: 1100px;
-  height: 100%;
-  margin: auto;
-}
-.header_first {
-  width: 10%;
-}
-.header_second {
-  width: 78%;
-}
-.navList_ul {
-  height: 100%
-}
-.navList_li {
-  height: 100%;
-  width: 20%;
-  line-height: 70px;
-  text-align: center;
-  cursor: pointer;
-}
-.header_third {
-  width: 10%;
 }
 </style>
