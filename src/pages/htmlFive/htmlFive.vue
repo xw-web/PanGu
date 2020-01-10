1835eeaebf8cce49e670fb7b0dc80f5bf58aaa3c
xw-web <1272716615@qq.com>
2020-01-10 16:31:54 +0800

    html5更改

--- src/pages/htmlFive/htmlFive.vue
+++ src/pages/htmlFive/htmlFive.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    <video width="320" height="240" controls="controls">
-      <source src="../../../static/video/movie.ogg" type="video/ogg" />
-      <source src="../../../static/video/movie.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
+    <div>
+      <video ref="video" width="320" height="240" controls="controls" >
+        <source src="../../../static/video/movie.ogg" type="video/ogg" />
+        <source src="../../../static/video/movie.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  mounted(){
+    console.log(this.$refs.video,'1')
+    this.$refs.video.autoplay = 'autoplay'
+    console.log(this.$refs.video,'2')
+  },
+  methods: {
+
+  }
+};
 </script>
 
 <style scoped>
